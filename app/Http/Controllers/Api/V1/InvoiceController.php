0469c7d53df22e263e1c23feed8a20ee1f3ffff3
quanphung0106-sudo <phungdinhquan16@gmail.com>
2023-08-03 14:29:04 +0700

    implement bulk insert

--- app/Http/Controllers/Api/V1/InvoiceController.php
+++ app/Http/Controllers/Api/V1/InvoiceController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Filters\V1\InvoiceFilter;
-use App\Http\Requests\StoreInvoiceRequest;
-use App\Http\Requests\UpdateInvoiceRequest;
 use App\Models\Invoice;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\V1\BulkStoreInvoiceRequest;
+use App\Http\Requests\V1\StoreInvoiceRequest;
+use App\Http\Requests\V1\UpdateInvoiceRequest;
 use App\Http\Resources\V1\InvoiceCollection;
 use App\Http\Resources\V1\InvoiceResource;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 
 class InvoiceController extends Controller
 {
@@ -43,6 +45,16 @@ class InvoiceController extends Controller
     public function store(StoreInvoiceRequest $request)
     {
         //
+    }
+
+    public function bulkStore(BulkStoreInvoiceRequest $request)
+    {
+        //Sử dụng collect() để chuyển mảng dữ liệu thành một collection để có thể áp dụng các phương thức và xử lý dữ liệu dễ dàng hơn.
+        $bulk = collect($request->all())->map(function ($arr, $key) {
+            //Lấy tất cả ngoại trừ 3 fields còn lại
+            return Arr::except($arr, ['customerId', 'billedDate', 'paidDate']);
+        });
+        Invoice::insert($bulk->toArray());
     }
 
     /**
