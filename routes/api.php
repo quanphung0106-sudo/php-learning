424912289ee5529f640908e82fb03f9638d7d8bd
quanphung0106-sudo <phungdinhquan16@gmail.com>
2023-08-03 16:23:22 +0700

    protecting routes with sanctum

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 //api/v1/
-Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], function () {
+Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1', 'middleware' => 'auth:sanctum'], function () {
 
     //Route::apiResource: tự động định nghĩa các route RESTful (GET, POST, PUT, DELETE) cho một controller. 
     Route::apiResource('customers', CustomerController::class);
